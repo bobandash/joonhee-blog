6ad4ccbfd4e36a92aa01ca836fe151a81c5d0e9b
Bruce Hsu <brucehsu1126@gmail.com>
2023-10-22 20:07:05 -0700

    add module.exports = router line

--- routes/mailing-list.js
+++ routes/mailing-list.js
@@ -4,3 +4,5 @@ const mailingListController = require('../controllers/mailingListController')
 
 router.post('/subscribe', mailingListController.subscribe_mailing_list);
 router.post('/unsubscribe/:emailId', mailingListController.unsubscribe_mailing_list)
+
+module.exports = router;

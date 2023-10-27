89d0cb689453de5530dc0303caeeab572d6a90f1
Bruce Hsu <brucehsu1126@gmail.com>
2023-10-26 23:08:25 -0700

    Create post page tsx and styling

---
+++ client-users/src/models/interface.ts
@@ -0,0 +1,12 @@
+export interface postProps {
+  title: string,
+  content: string,
+  isVisible: boolean,
+  dateFormatted: string
+}
+
+export interface commentsProps {
+  username: string,
+  message: string,
+  dateFormatted: string
+}
--- client-users/src/utils/Router.tsx
+++ client-users/src/utils/Router.tsx
@@ -1,11 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from './App.tsx'
+import PostPage from "../pages/Posts/PostPage.tsx"
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <App/>,
+    },
+/*     {
+      path: "/posts",
+      element: <AllPostsPage />
+    },  */
+    {
+      path: "/:postId",
+      element: <PostPage />
     }
   ])
 

3ce138b4e0ecf246d441eda8587b5bd88ec358f8
Bruce Hsu <brucehsu1126@gmail.com>
2023-10-28 15:55:21 -0700

    Add loading screen

---
+++ client-users/src/pages/LoadingScreen/LoadingScreen.tsx
@@ -0,0 +1,32 @@
+import { PacmanLoader } from "react-spinners";
+import { CSSProperties } from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+function LoadingScreen() {
+  const override: CSSProperties = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    margin: "0 auto",
+    borderColor: "red",
+  };
+
+  return (
+    <>
+      <Header />
+      <PacmanLoader
+        color={"#009959"}
+        loading={true}
+        cssOverride={override}
+        size={30}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+      <Footer />
+    </>
+  );
+}
+
+export default LoadingScreen;

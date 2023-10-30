188c8e037468d6be26e56943f140bc5429f1bb75
Bruce Hsu <brucehsu1126@gmail.com>
2023-10-29 17:29:02 -0700

    Add error page

---
+++ client-users/src/pages/Error/ErrorPage.tsx
@@ -0,0 +1,29 @@
+import {FC} from 'react'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import styles from './ErrorPage.module.css'
+
+interface ErrorPageProps {
+  errorStatus?: number
+}
+
+const ErrorPage:FC<ErrorPageProps> = ({errorStatus = 404}) => {
+  function redirectHome(){
+    window.location.href = "/"
+  }
+  return(
+    <>
+    <Header />
+    <div className = {styles["error-container"]}>
+      {errorStatus === 404 ? 
+        <h1>Page does not exist</h1> :
+        <h1>Internal Server Error</h1>
+      }
+      <button onClick = {redirectHome}>Go Back Home <i className = "fa-solid fa-house"></i></button>
+    </div>
+    <Footer />
+    </>
+  )
+}
+
+export default ErrorPage

2e33efdf84a9afd9f9313f32c8014f0d006a3be1
Bruce Hsu <brucehsu1126@gmail.com>
2023-10-29 22:21:25 -0700

    Add mailing list style and functionality

--- client-users/src/pages/Home/Notification.tsx
+++ client-users/src/pages/Home/Notification.tsx
@@ -1,13 +1,61 @@
+import ErrorPage from '../Error/ErrorPage';
 import styles from './Notification.module.css'
+import {useState} from 'react'
 
 function Notification(){
+  const [hasError, setHasError] = useState(false);
+  const [subscribed, setSubscribed] = useState(false);
+  const [hasInvalidEmail, setHasInvalidEmail] = useState(false);
+  const [email, setEmail] = useState('');
+
+  async function registerEmail(){
+    try {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_PORT}/mailing/subscribe`, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+          'email': email
+        })
+      })
+      if(!response.ok){
+        setHasInvalidEmail(true);
+      } else {
+        setSubscribed(true);
+      }
+    } catch {
+      setHasError(true);
+    }
+  }
+
+  if(hasError){
+    <ErrorPage errorStatus = {500} />
+  }
+
   return (
     <section className = {styles["section-background"]}>
-      <form className = {styles["email-newsletter"]}>
+      <form className = {styles["email-newsletter"]} noValidate>
         <label htmlFor = "email">Join My Journey</label>
-        <div className = {styles["input-and-button"]}>
-          <input type = "email" name = "email" id = "email"/>
-          <button>Subscribe</button>
+        <div className = {styles["form-elem-container"]}>
+          <div className = {styles["input-and-button"]}>
+            <input onChange = {(e) => {
+              setSubscribed(false);
+              setHasInvalidEmail(false);
+              setEmail(e.currentTarget.value);
+            }} placeholder= "Enter your email" type = "email" name = "email" id = "email" autoComplete='off'/>
+            <button onClick = {
+              async (e) => {
+                e.preventDefault();
+                await registerEmail();
+              }}>
+                Subscribe
+            </button>
+        </div>
+          {subscribed && <span className = {`${styles.message} success`}>You have successfully subscribed.</span>}
+          {hasInvalidEmail && <span className = {`${styles.message} error`}>Your email is invalid. Please try again.</span>}
         </div>
       </form>
     </section>

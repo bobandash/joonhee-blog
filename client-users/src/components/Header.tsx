2c516526dbdf9e126de9c1b0480eed27c35083a6
Bruce Hsu <brucehsu1126@gmail.com>
2023-10-29 19:49:02 -0700

    Change Header styling to black and svg logo

--- client-users/src/components/Header.tsx
+++ client-users/src/components/Header.tsx
@@ -1,16 +1,18 @@
 
 import styles from './Header.module.css'
+import JoonheeLogo from '../assets/joonhee-logo.svg'
 
 function Header(){
   return (
     <header className = {styles["header"]}>
       <div className = {styles["container"]}>
         <p className = {styles["name"]}>
-          <a href = "/">Joonhee Bock</a>
+          <a className = {styles["home-redirect"]} href = "/"><img className = {styles["joonhee-logo"]} src = {JoonheeLogo} alt = "Joonhee logo" /></a>
         </p>
         <ul className = {styles["nav-items"]}>
           <li><a href="/about">About</a></li>
-          <li><a href="">Posts</a></li>
+          <li><a href="/posts">Posts</a></li>
+          <li><a href="/contact">Contact</a></li>
         </ul>
       </div>
     </header>

2b59ca22c1e4041a4696c4ebaf0be0c8ed54a8e4
Bruce Hsu <brucehsu1126@gmail.com>
2023-10-30 19:25:45 -0700

    Add links and styling to footer

--- client-users/src/components/Footer.tsx
+++ client-users/src/components/Footer.tsx
@@ -6,10 +6,10 @@ function Footer(){
     <footer className = {styles["footer"]}>
       <div className = {styles["container"]}>
         <ul className = {styles["nav-items"]}>
-          <li><a href="">About</a></li>
-          <li><a target= "_blank" href="">LetterBoxd</a></li>
-          <li><a href="">Posts</a></li>
-          <li><a href="">Contact</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a target= "_blank" href="https://letterboxd.com/tada/">LetterBoxd</a></li>
+          <li><a href="/posts">Posts</a></li>
+          <li><a href="/contact">Contact</a></li>
         </ul>
       </div>
     </footer>

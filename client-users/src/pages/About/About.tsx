250a7824f19767947ad89959da58848d19e211d4
Bruce Hsu <brucehsu1126@gmail.com>
2023-12-13 13:56:11 -0800

    Change about me page

--- client-users/src/pages/About/About.tsx
+++ client-users/src/pages/About/About.tsx
@@ -1,24 +1,37 @@
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import styles from './About.module.css'
+import styles from "./About.module.css";
 const About = () => {
-  return(
+  return (
     <>
       <Header />
-      <div className= {`container ${styles["about-container"]}`}>
-        <h1 className = 'section-title'>About</h1>
-        <div className = {styles.content}>
+      <div className={`container ${styles["about-container"]}`}>
+        <h1 className="section-title">About</h1>
+        <div className={styles.content}>
           <div>
-            <p>I'm Joonhee Bock, and my greatest dreams and aspirations revolve around teaching children and leaving a lasting impact on the world. I firmly believe that nurturing young minds is the most profound way to shape a better future.  My love for the world of cinema is unmatched, with every frame, dialogue, and story leaving an indelible mark on my heart. Whether I'm exploring the depth of character development, analyzing plot twists, or simply enjoying the magic of the silver screen, cinema is a constant source of inspiration and wonder for me.</p>
-            <p>My academic journey took me through the hallowed halls of Notre Dame University, where I earned a bachelor’s degree in economics and education, complemented by a minor in Korean. This academic foundation has equipped me with the knowledge and skills to empower the next generation and contribute positively to society.  Currently, I find myself in Kosovo, where I'm passionately teaching English to Kosovar children. This experience is the realization of my dreams, as I work to ignite the spark of knowledge and curiosity in these young minds. Every day, I'm reminded of the profound impact education can have on shaping lives and building a brighter future.</p>
-            <p>While I have many other interests, including indulging in video games and appreciating the talents of female musicians and artists like Laufey and Tzuyu from Twice, my true calling lies in the classroom, fostering a love for learning in the hearts of the next generation. Through education and the magic of cinema, I hope to leave a meaningful mark on the world.</p> 
-            <p>If you have any questions about my journey or want to connect, please feel free to <a href = "/contact">contact me here</a>.</p>
+            <p>
+              Hi! My name is Joonhee Bock and I recently graduated from
+              University of Notre Dame, majoring in Economics and Education. I
+              am currently a Peace Corps volunteer in Kosovo as a TEFL
+              volunteer. I love to take photos, read, make music, and watch
+              movies.
+            </p>
+
+            <p>
+              If you have any questions about my journey or want to connect,
+              please feel free to <a href="/contact">contact me here</a>.
+            </p>
+            <p>
+              The content of this website is mine alone and does not reflect the
+              views of the U.S. Government, the Peace Corps, or the Republic of
+              Kosovo Government.
+            </p>
           </div>
         </div>
       </div>
       <Footer />
     </>
-  )
-}
+  );
+};
 
 export default About;

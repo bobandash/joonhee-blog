04849026c9ce4c96f16cbef3ea6ba9baf03fd994
Bruce Hsu <brucehsu1126@gmail.com>
2023-12-14 13:58:38 -0800

    Test potential fix to timezone

--- client-users/src/models/interface.ts
+++ client-users/src/models/interface.ts
@@ -1,14 +1,15 @@
 export interface postProps {
-  title: string,
-  content: string,
-  summary: string,
-  isVisible: boolean,
-  dateFormatted: string,
-  id: string
+  title: string;
+  content: string;
+  summary: string;
+  isVisible: boolean;
+  dateFormatted: string;
+  id: string;
 }
 
 export interface commentsProps {
-  username: string,
-  message: string,
-  dateFormatted: string
+  username: string;
+  message: string;
+  dateFormatted: string;
+  timestamp: Date;
 }
--- client-users/src/pages/IndividualPost/Comment.tsx
+++ client-users/src/pages/IndividualPost/Comment.tsx
@@ -1,24 +1,35 @@
-import { commentsProps } from "../../models/interface"
-import {FC} from 'react'
-import styles from './Comment.module.css'
-import he from 'he'
+import { commentsProps } from "../../models/interface";
+import { FC } from "react";
+import styles from "./Comment.module.css";
+import he from "he";
 
 interface CommentProps {
-  comment: commentsProps
+  comment: commentsProps;
 }
 
-const CommentComponent: FC<CommentProps>= ({comment}) => {
-  return(
-    <div className = {styles["comment-wrapper"]}>
-      <div className = {styles["comment"]}>
-        <div className = {styles["same-line"]}>
-          <p className = {styles.username}>{he.decode(comment.username)}</p>
-          <p className = {styles.date}>{he.decode(comment.dateFormatted)}</p>
+const CommentComponent: FC<CommentProps> = ({ comment }) => {
+  const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  console.log(userTimeZone);
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "numeric",
+    minute: "numeric",
+    timeZone: userTimeZone || "UTC",
+  }).format(comment.timestamp);
+
+  return (
+    <div className={styles["comment-wrapper"]}>
+      <div className={styles["comment"]}>
+        <div className={styles["same-line"]}>
+          <p className={styles.username}>{he.decode(comment.username)}</p>
+          <p className={styles.date}>{formattedDate}</p>
         </div>
-        <p className = {styles.message}>{he.decode(comment.message)}</p>
+        <p className={styles.message}>{he.decode(comment.message)}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CommentComponent
+export default CommentComponent;

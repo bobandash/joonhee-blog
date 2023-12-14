6738f9e782084a8f6043a14cbee07a56e31f672f
Bruce Hsu <brucehsu1126@gmail.com>
2023-12-14 00:53:52 -0800

    Fix contact me route

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -1,33 +1,20 @@
-const ContactMessages = require('../models/contact');
-const {body, validationResult} = require('express-validator');
-const asyncHandler = require('express-async-handler');
+const ContactMessages = require("../models/contact");
+const { body, validationResult } = require("express-validator");
+const asyncHandler = require("express-async-handler");
 
 exports.create_message = [
-  body('email', 'Must be a valid email')
-    .trim()
-    .isEmail()
-    .escape(),
-  body('subjectLine', 'Message cannot be empty')
-    .trim()
-    .notEmpty()
-    .escape(),
-  body('message', 'Message cannot be empty')
-    .trim()
-    .notEmpty()
-    .escape(),
+  body("email", "Must be a valid email").trim().isEmail().escape(),
+  body("subjectLine", "Message cannot be empty").trim().notEmpty().escape(),
+  body("message", "Message cannot be empty").trim().notEmpty().escape(),
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req).mapped();
-    if(Object.keys(errors).length > 0){
-      res.send(400).json(errors);
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
     } else {
-      const [email, subjectLine, message] = [
-        req.params.email,
-        req.body.subjectLine,
-        req.body.message
-      ]
-      const newMessage = new ContactMessages({email, subjectLine, message});
+      const { email, subjectLine, message } = req.body;
+      const newMessage = new ContactMessages({ email, subjectLine, message });
       await newMessage.save();
-      res.json(newMessage);
+      return res.status(200).json(newMessage);
     }
-  })
-]
+  }),
+];

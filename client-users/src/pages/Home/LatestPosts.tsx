d3dc8303afefae708ce6c7292ea5ab7633be2f4c
Bruce Hsu <brucehsu1126@gmail.com>
2023-12-14 02:12:41 -0800

    Change background of latest posts

--- client-users/src/pages/Home/LatestPosts.tsx
+++ client-users/src/pages/Home/LatestPosts.tsx
@@ -1,26 +1,25 @@
-import styles from './LatestPosts.module.css'
-import LatestPost from './LatestPost'
-import {FC} from 'react';
-import { postProps } from '../../models/interface';
+import styles from "./LatestPosts.module.css";
+import LatestPost from "./LatestPost";
+import { FC } from "react";
+import { postProps } from "../../models/interface";
 
 interface LatestPostsProps {
-  posts: postProps[]
+  posts: postProps[];
 }
 
-const LatestPosts:FC<LatestPostsProps> = ({posts}) => {
-  return(
-    <section className = {styles["latest-posts"]}>
-      <div className = {styles["container"]}>
-        <h1 className = {styles["section-header"]}>Latest Posts</h1>
-        <div className = {styles["posts-container"]}>
-          {posts.map(post => (
-            <LatestPost key = {post.id} post = {post} />
+const LatestPosts: FC<LatestPostsProps> = ({ posts }) => {
+  return (
+    <section>
+      <div className={styles["container"]}>
+        <h1 className={styles["section-header"]}>Latest Posts</h1>
+        <div className={styles["posts-container"]}>
+          {posts.map((post) => (
+            <LatestPost key={post.id} post={post} />
           ))}
         </div>
       </div>
     </section>
-
-  )
-}
+  );
+};
 
 export default LatestPosts;
